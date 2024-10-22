2e1f51d1078ace64a185b75332652f1b4a4f3b7c
unknown <alexandra.krutihina@ya.ru>
2024-10-22 22:49:28 +0300

    Second commit!

--- app.py
+++ app.py
@@ -1,23 +1,87 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Conversion functions
+def convert_length(value, from_unit, to_unit):
+    length_units = {
+        'mm': 0.001,
+        'cm': 0.01,
+        'm': 1,
+        'km': 1000,
+        'in': 0.0254,
+        'ft': 0.3048,
+        'yd': 0.9144,
+        'mi': 1609.34
+    }
+    return value * (length_units[from_unit] / length_units[to_unit])
+
+def convert_weight(value, from_unit, to_unit):
+    weight_units = {
+        'mg': 0.001,
+        'g': 1,
+        'kg': 1000,
+        'oz': 28.3495,
+        'lb': 453.592
+    }
+    return value * (weight_units[from_unit] / weight_units[to_unit])
+
+def convert_temperature(value, from_unit, to_unit):
+    if from_unit == 'C':
+        if to_unit == 'F':
+            return (value * 9/5) + 32
+        elif to_unit == 'K':
+            return value + 273.15
+        else:
+            return value
+    elif from_unit == 'F':
+        if to_unit == 'C':
+            return (value - 32) * 5/9
+        elif to_unit == 'K':
+            return (value - 32) * 5/9 + 273.15
+        else:
+            return value
+    elif from_unit == 'K':
+        if to_unit == 'C':
+            return value - 273.15
+        elif to_unit == 'F':
+            return (value - 273.15) * 9/5 + 32
+        else:
+            return value
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return redirect(url_for('length'))  # Redirect to length converter
 
-@app.route('/length')
+@app.route('/length', methods=['GET', 'POST'])
 def length():
-    return render_template('length.html')
+    result = None
+    if request.method == 'POST':
+        value = float(request.form['value'])
+        from_unit = request.form['from_unit']
+        to_unit = request.form['to_unit']
+        result = convert_length(value, from_unit, to_unit)
+    return render_template('length.html', result=result)
 
-@app.route('/weight')
+@app.route('/weight', methods=['GET', 'POST'])
 def weight():
-    return render_template('weight.html')
+    result = None
+    if request.method == 'POST':
+        value = float(request.form['value'])
+        from_unit = request.form['from_unit']
+        to_unit = request.form['to_unit']
+        result = convert_weight(value, from_unit, to_unit)
+    return render_template('weight.html', result=result)
 
-@app.route('/temperature')
+@app.route('/temperature', methods=['GET', 'POST'])
 def temperature():
-    return render_template('temperature.html')
+    result = None
+    if request.method == 'POST':
+        value = float(request.form['value'])
+        from_unit = request.form['from_unit']
+        to_unit = request.form['to_unit']
+        result = convert_temperature(value, from_unit, to_unit)
+    return render_template('temperature.html', result=result)
 
-
-if  __name__ == '__main__':
-    app.run(port=5001, debug=True)
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
